aa6a0878fb62d8b14a318677a656abaf95379e15
Daniel Lacasse <daniel@nokee.dev>
2024-11-04 20:14:05 -0500

    Introduce Commons Hamcrest for all assertion needs
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

---
+++ commons-hamcrest/src/main/java/dev/nokee/commons/hamcrest/gradle/BuildDependenciesMatcher.java
@@ -0,0 +1,22 @@
+package dev.nokee.commons.hamcrest.gradle;
+
+import org.gradle.api.Buildable;
+import org.gradle.api.Task;
+import org.hamcrest.FeatureMatcher;
+import org.hamcrest.Matcher;
+
+public class BuildDependenciesMatcher extends FeatureMatcher<Buildable, Iterable<Task>> {
+	public BuildDependenciesMatcher(Matcher<? super Iterable<Task>> matcher) {
+		super(matcher, "a buildable object with dependencies", "buildable object with dependencies");
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	protected Iterable<Task> featureValueOf(Buildable actual) {
+		return (Iterable<Task>) actual.getBuildDependencies().getDependencies(null);
+	}
+
+	public static BuildDependenciesMatcher buildDependencies(Matcher<? super Iterable<Task>> matcher) {
+		return new BuildDependenciesMatcher(matcher);
+	}
+}
